8d98094e245e7359eb4e7a19c479c03d48cca8c3
DadKnowsAI <dadknowsai@gmail.com>
2025-09-04 01:01:44 +0000

    Add GA4 global tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <title>DadKnowsAI</title>
         <meta name="description" content="Beta chatbot with GPT-5" />
+        <title>DadKnowsAI</title>
       </head>
       <body>{children}</body>
     </html>
