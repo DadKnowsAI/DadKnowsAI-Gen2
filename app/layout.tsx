8a8938922856b3281bd32d10b7593e973ddd31f1
DadKnowsAI <dadknowsai@gmail.com>
2025-09-04 00:55:08 +0000

    Wire GA4 in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
+        {/* Google Analytics 4 */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZLLW3P8YJZ"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -21,10 +21,14 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-ZLLW3P8YJZ', { send_page_view: true });
+              gtag('config', 'G-ZLLW3P8YJZ', { send_page_view: true, debug_mode: true });
             `,
           }}
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>DadKnowsAI</title>
+        <meta name="description" content="Beta chatbot with GPT-5" />
       </head>
       <body>{children}</body>
     </html>
